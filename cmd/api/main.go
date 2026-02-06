3fda57aad384a8ffea700a798506a1429d5927f3
Haile <hailecst@gmail.com>
2026-02-06 19:16:38 +0300

    using env file for address

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
+)
 
 func main() {
 
+	// u can load multiple files
+	err := godotenv.Load(".env", ".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	addr := os.Getenv("GOPHER_SOCIAL_ADDR")
+
 	cfg := config{
-		addr: ":8080",
+		addr: addr,
 	}
 
 	app := &application{
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/high-la/gopher-social
 
 go 1.25.5
 
-require github.com/go-chi/chi/v5 v5.2.5
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/joho/godotenv v1.5.1
+)
