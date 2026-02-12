b47b08257b94193f21cf0d5ab38f80a4be638272
Haile <hailecst@gmail.com>
2026-02-12 17:26:39 +0300

    authorization database setup

---
+++ cmd/migrate/migrations/000012_add_roles_table.up.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS roles (
+
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  level int NOT NULL DEFAULT 0,
+  description TEXT
+);
+
+-- user
+INSERT INTO roles 
+    (name, description, level)
+VALUES 
+    ('user', 'A user can create posts and comments', 1 );
+
+-- Moderator
+INSERT INTO roles 
+    (name, description, level)
+VALUES
+  ('moderator', 'A moderator can update other users posts', 2);
+
+-- Admin
+INSERT INTO roles 
+    (name, description, level)
+VALUES
+  ('admin', 'An admin can update and delete other users posts', 3);
+  
+  
+COMMIT;
